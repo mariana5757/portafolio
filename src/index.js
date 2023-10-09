7a25adc5ea76fb80704857d4bb2842597bb0e651
mariana <molondono57@gmail.com>
2023-10-09 10:21:59 -0500

    dic principal y skills iniciadas

--- src/index.js
+++ src/index.js
@@ -1,17 +1,61 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
+const name = ReactDOM.createRoot(document.getElementById('name'))
+const information = ReactDOM.createRoot(document.getElementById('information'))
+const django = ReactDOM.createRoot(document.getElementById('django'))
+const react_div = ReactDOM.createRoot(document.getElementById('react_div'))
+
+function Image() {
+    return <div>
+        <img class="picture" src="/foto.jpg" width='90%'alt="Foto de mariana" />
+    </div>
+}
+
+function Name() {
+    return <div>
+        <h1>Mariana Ortiz</h1>
+    </div>
+}
+
+function Information() {
+    return <div>
+        <h3>Estudiante de analisis y desarrollo de software</h3>
+        <p>Apasionada por el aprendizaje, el desarrollo de software y a los nuevos retos.</p>
+        <a href="https://github.com/mariana5757">
+        <button class="btn btn-primary" >GitHub</button></a>
+    </div>
+}
+
+function Django() {
+    return <div>
+        <img src="/django.png" width='50%' height='50%' alt="logo django" />
+        <p>Habilidades en el framework Django</p>
+    </div>
+}
+
+function ReactLogo(){
+    return <div>
+        <img src="/react.png" width='50%' height='50%'  alt="logo react" />
+        <p>Conocimiento básico en React</p>
+    </div>
+}
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    <Image />
+)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+name.render(
+    <Name />
+)
+
+information.render(
+    <Information />
+)
+django.render(
+    <Django/>
+)
+react_div.render(
+    <ReactLogo/>
+)
